e92bddf9f6ddf53b0913d8f8e84a702ba23d8539
Hajin Kim <rhrnak624@naver.com>
2022-02-20 23:46:32 +0900

    초기 퀴즈 데이터 DB에 추가

--- QuizQuiz/app/src/main/java/wikibook/learnandroid/quizquiz/QuizMainActivity.kt
+++ QuizQuiz/app/src/main/java/wikibook/learnandroid/quizquiz/QuizMainActivity.kt
@@ -1,5 +1,8 @@
 package wikibook.learnandroid.quizquiz
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -7,15 +10,32 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.loader.content.AsyncTaskLoader
 import com.google.android.material.internal.NavigationMenu
 import com.google.android.material.navigation.NavigationView
+import org.w3c.dom.Element
+import wikibook.learnandroid.quizquiz.database.Quiz
+import wikibook.learnandroid.quizquiz.database.QuizDatabase
+import javax.xml.parsers.DocumentBuilderFactory
 
 class QuizMainActivity : AppCompatActivity() {
     lateinit var drawerToggle: ActionBarDrawerToggle
+    lateinit var db: QuizDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quiz_main_activity)
+
+        db = QuizDatabase.getInstance(this)
+
+        val sp : SharedPreferences = this.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        if(sp.getBoolean("initialized", true)) {
+            initQuizDataFromXMLFile()
+
+            val editor = sp.edit()
+            editor.putBoolean("initialized", false)
+            editor.commit()
+        }
 
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navView = findViewById<NavigationView>(R.id.drawer_nav_view)
@@ -50,5 +70,43 @@ class QuizMainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun initQuizDataFromXMLFile() {
+        AsyncTask.execute {
+            val stream = assets.open("quizzes.xml")
+
+            val docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+
+            val doc = docBuilder.parse(stream)
+
+            val quizzesFromXMLDoc = doc.getElementsByTagName("quiz")
+
+            val quizList = mutableListOf<Quiz>()
+            for(idx in 0 until quizzesFromXMLDoc.length) {
+                val e = quizzesFromXMLDoc.item(idx) as Element
+                val type = e.getAttribute("type")
+                val question = e.getElementsByTagName("question").item(0).textContent
+                val answer = e.getElementsByTagName("answer").item(0).textContent
+                val category = e.getElementsByTagName("category").item(0).textContent
+
+                when {
+                    type == "ox"-> {
+                        quizList.add(Quiz(type=type, question = question, answer=answer, category=category))
+                    }
+                    type == "multiple_choice" -> {
+                        val choices = e.getElementsByTagName("choice")
+                        val choiceList = mutableListOf<String>()
+                        for(idx in 0 until choices.length) {
+                            choiceList.add(choices.item(idx).textContent)
+                        }
+                        quizList.add(Quiz(type=type, question = question, answer=answer, category=category, guesses = choiceList))
+                    }
+                }
+            }
+            for(quiz in quizList) {
+                db.quizDAO().insert(quiz)
+            }
+        }
     }
 }
