69c75b78774639b4cf846dadb782fb3cc6836399
Hajin Kim <rhrnak624@naver.com>
2022-02-26 04:03:12 +0900

    카드뷰 & ratingBar

--- NaverMovieAPIApp/app/src/main/java/wikibook/learnandroid/navermovieapiapp/MovieAdapter.kt
+++ NaverMovieAPIApp/app/src/main/java/wikibook/learnandroid/navermovieapiapp/MovieAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,10 +20,10 @@ class MovieAdapter(private val datalist: List<Movie>, val ctx: Context) :Recycle
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         var movieTitle = holder.view.findViewById<TextView>(R.id.movie_title)
-        var movieRating = holder.view.findViewById<TextView>(R.id.movie_rating)
+        var movieRating = holder.view.findViewById<RatingBar>(R.id.movie_rating)
 
         movieTitle.text = datalist[position].title
-        movieRating.text = datalist[position].rating.toString()
+        movieRating.rating = datalist[position].rating!!.toFloat()
     }
 
     override fun getItemCount(): Int {
